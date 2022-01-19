ecb301b96f062c088824a725829df3165bf4f48f
Kyle <Kyle.Plummer@revature.com>
2022-01-19 15:54:01 -0600

    SQL script

---
+++ script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE associates (
+    associate_id INT NOT NULL,
+    first_name VARCHAR(200),
+    last_name VARCHAR(200),
+    age INT,
+    CONSTRAINT associates_pk PRIMARY KEY (associate_id)
+);
